e1b295d60240ac4cc3366b94f321274290c053e6
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-07-10 11:25:08 -0400

    Update

---
+++ helloCalt.c
@@ -0,0 +1,16 @@
+
+
+// Include the standard input-output library for functions like printf
+#include <stdio.h>
+
+// Include the standard library for general purpose functions
+#include <stdlib.h>
+
+// Main function where the execution of the program begins
+int main(void) {
+    // Print the string "Hello, C!" followed by a new line to the console
+    printf("Hello, C!\n");
+    
+    // Return 0 to indicate that the program ended successfully
+    return 0;
+}
