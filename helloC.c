8551b50e0e8feffbba497d06101982366374ae92
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-07-09 21:00:46 -0400

    Add files via upload

---
+++ helloC.c
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Author: Nicholas Fisher
+// Date: July 9th 2024
+// Description of the Script:
+//  Basic Hello World! Script
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>   // Including standard input-output library header
+#include <stdlib.h>  // Including standard library header for EXIT_SUCCESS
+
+int main(void) {     // Starting point of the program, main function with no arguments
+    puts("Hello C!");  // puts function is used to output a string to the console
+    return EXIT_SUCCESS;  // Returning EXIT_SUCCESS to indicate successful execution
+}
